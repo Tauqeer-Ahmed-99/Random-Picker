b7f1d01ebc61535378e43a9e0fa4d7972460d98a
Tauqeer Khan <tauqueerrkhan@gmail.com>
2023-01-20 12:01:41 +0530

    Initial Commit

---
+++ src/Animation.js
@@ -0,0 +1,191 @@
+import React from "react";
+
+import "./animation.css";
+
+const Animation = () => {
+  return (
+    <div class="fireworks">
+      <div class="firework">
+        <div class="explosion">
+          <div class="spark gold"></div>
+        </div>
+        <div class="explosion">
+          <div class="spark gold"></div>
+        </div>
+        <div class="explosion">
+          <div class="spark gold"></div>
+        </div>
+        <div class="explosion">
+          <div class="spark gold"></div>
+        </div>
+        <div class="explosion">
+          <div class="spark gold"></div>
+        </div>
+        <div class="explosion">
+          <div class="spark gold"></div>
+        </div>
+        <div class="explosion">
+          <div class="spark gold"></div>
+        </div>
+        <div class="explosion">
+          <div class="spark gold"></div>
+        </div>
+      </div>
+      <div class="firework" style={{ marginTop: "-70px" }}>
+        <div class="explosion">
+          <div class="spark silver"></div>
+        </div>
+        <div class="explosion">
+          <div class="spark silver"></div>
+        </div>
+        <div class="explosion">
+          <div class="spark silver"></div>
+        </div>
+        <div class="explosion">
+          <div class="spark silver"></div>
+        </div>
+        <div class="explosion">
+          <div class="spark silver"></div>
+        </div>
+        <div class="explosion">
+          <div class="spark silver"></div>
+        </div>
+        <div class="explosion">
+          <div class="spark silver"></div>
+        </div>
+        <div class="explosion">
+          <div class="spark silver"></div>
+        </div>
+      </div>
+      <div class="firework">
+        <div class="explosion">
+          <div class="spark gold"></div>
+        </div>
+        <div class="explosion">
+          <div class="spark gold"></div>
+        </div>
+        <div class="explosion">
+          <div class="spark gold"></div>
+        </div>
+        <div class="explosion">
+          <div class="spark gold"></div>
+        </div>
+        <div class="explosion">
+          <div class="spark gold"></div>
+        </div>
+        <div class="explosion">
+          <div class="spark gold"></div>
+        </div>
+        <div class="explosion">
+          <div class="spark gold"></div>
+        </div>
+        <div class="explosion">
+          <div class="spark gold"></div>
+        </div>
+      </div>
+      <div class="firework" style={{ marginTop: "-70px" }}>
+        <div class="explosion">
+          <div class="spark silver"></div>
+        </div>
+        <div class="explosion">
+          <div class="spark silver"></div>
+        </div>
+        <div class="explosion">
+          <div class="spark silver"></div>
+        </div>
+        <div class="explosion">
+          <div class="spark silver"></div>
+        </div>
+        <div class="explosion">
+          <div class="spark silver"></div>
+        </div>
+        <div class="explosion">
+          <div class="spark silver"></div>
+        </div>
+        <div class="explosion">
+          <div class="spark silver"></div>
+        </div>
+        <div class="explosion">
+          <div class="spark silver"></div>
+        </div>
+      </div>
+      <div class="firework">
+        <div class="explosion">
+          <div class="spark gold"></div>
+        </div>
+        <div class="explosion">
+          <div class="spark gold"></div>
+        </div>
+        <div class="explosion">
+          <div class="spark gold"></div>
+        </div>
+        <div class="explosion">
+          <div class="spark gold"></div>
+        </div>
+        <div class="explosion">
+          <div class="spark gold"></div>
+        </div>
+        <div class="explosion">
+          <div class="spark gold"></div>
+        </div>
+        <div class="explosion">
+          <div class="spark gold"></div>
+        </div>
+        <div class="explosion">
+          <div class="spark gold"></div>
+        </div>
+      </div>
+      <div class="firework" style={{ marginTop: "-70px" }}>
+        <div class="explosion">
+          <div class="spark silver"></div>
+        </div>
+        <div class="explosion">
+          <div class="spark silver"></div>
+        </div>
+        <div class="explosion">
+          <div class="spark silver"></div>
+        </div>
+        <div class="explosion">
+          <div class="spark silver"></div>
+        </div>
+        <div class="explosion">
+          <div class="spark silver"></div>
+        </div>
+        <div class="explosion">
+          <div class="spark silver"></div>
+        </div>
+        <div class="explosion">
+          <div class="spark silver"></div>
+        </div>
+        <div class="explosion">
+          <div class="spark silver"></div>
+        </div>
+      </div>
+      <div class="firework">
+        <div class="explosion">
+          <div class="spark gold"></div>
+        </div>
+        <div class="explosion">
+          <div class="spark gold"></div>
+        </div>
+        <div class="explosion">
+          <div class="spark gold"></div>
+        </div>
+        <div class="explosion">
+          <div class="spark gold"></div>
+        </div>
+        <div class="explosion">
+          <div class="spark gold"></div>
+        </div>
+        <div class="explosion">
+          <div class="spark gold"></div>
+        </div>
+        <div class="explosion">
+          <div class="spark gold"></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Animation;
--- src/App.js
+++ src/App.js
@@ -1,22 +1,128 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
+import Animation from "./Animation";
 
 function App() {
+  const [winner, setWinner] = useState("");
+  const [participantName, setParticipantName] = useState("");
+  const [participants, setParticipants] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [showWinner, setShowWinner] = useState(false);
+
+  const handleParticipantNameChange = (e) => {
+    setParticipantName(e.target.value);
+  };
+
+  const handleAddParticipant = () => {
+    const isAlreadyInParticipants = !!participants.filter(
+      (participant) =>
+        participant.toLowerCase() === participantName.toLowerCase()
+    ).length;
+
+    if (isAlreadyInParticipants) {
+      alert(`${participantName} is already in participants list.`);
+    } else {
+      if (participantName) {
+        setParticipants((prevParticipants) => [
+          ...prevParticipants,
+          participantName,
+        ]);
+        setParticipantName("");
+      } else {
+        alert("Please enter a valid participant name.");
+      }
+    }
+  };
+
+  const start = () => {
+    setIsLoading(true);
+    setTimeout(() => {
+      const winner =
+        participants[Math.floor(Math.random() * participants.length)];
+      setIsLoading(false);
+      setWinner(winner);
+      setShowWinner(true);
+    }, 5000);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {showWinner && (
+          <>
+            <div className={isLoading ? "" : "winner"}>
+              <div>Winner</div>
+              <h1>{isLoading ? "Thinking..." : winner}</h1>
+            </div>
+            <button
+              style={{
+                marginLeft: "2rem",
+                height: "2rem",
+                cursor: "pointer",
+                position: "fixed",
+                right: "2rem",
+                bottom: "2rem",
+              }}
+              onClick={() => setShowWinner(false)}
+            >
+              Restart
+            </button>
+            <Animation />
+          </>
+        )}
+        {!showWinner &&
+          (isLoading ? (
+            <>
+              <img src={logo} className="App-logo" alt="logo" />
+              <div>Any guess?? {participants.join(", ")}?</div>
+            </>
+          ) : (
+            <>
+              <div>
+                <input
+                  value={participantName}
+                  onChange={handleParticipantNameChange}
+                  style={{ height: "2rem", width: "20rem", fontSize: "1rem" }}
+                />
+                <button
+                  style={{
+                    marginLeft: "2rem",
+                    height: "2rem",
+                    width: "10rem",
+                    cursor: "pointer",
+                  }}
+                  onClick={handleAddParticipant}
+                >
+                  Add Participant
+                </button>
+              </div>
+              <div>
+                <div>
+                  {!participants.length && <div>Add participants!</div>}
+                  {participants.length > 0 && (
+                    <div style={{ marginBottom: "2rem" }}>Participants</div>
+                  )}
+                  {participants.map((participant) => (
+                    <div key={participant}>{participant}</div>
+                  ))}
+                </div>
+                {participants.length > 0 && (
+                  <button
+                    onClick={start}
+                    style={{
+                      marginLeft: "2rem",
+                      height: "2rem",
+                      width: "10rem",
+                      cursor: "pointer",
+                    }}
+                  >
+                    Start
+                  </button>
+                )}
+              </div>
+            </>
+          ))}
       </header>
     </div>
   );
